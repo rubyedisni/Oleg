87c32527300799d943a6b3fb84c983fb5e4ae074
rubyedisni <62126791+rubyedisni@users.noreply.github.com>
2020-04-08 20:44:09 +0500

    bruh

---
+++ New Unity Project/Assets/scripts/go1.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class go1 : MonoBehaviour
+{
+    public bool back;
+    public float timer;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if(!back)
+        {
+            transform.Rotate (Vector3.up * 450 * Time.deltaTime);
+            if(timer >= 3)
+            {
+                back = true;
+                timer = 0;
+            }
+        }
+        else if (back)
+        {
+            transform.Rotate (Vector3.up * -450 * Time.deltaTime);
+            if(timer >= 3)
+            {
+                back = false;
+                timer = 0;
+            }
+
+        }
+    }
+}
